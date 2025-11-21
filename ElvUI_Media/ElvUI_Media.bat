6931ff8459497a4a8339cfe2ce644e6e41636523
Heather <42354237+IHealDrunk@users.noreply.github.com>
2025-11-21 13:13:35 -0600

    Fixed Batch not adding closure................. I hope

--- ElvUI_Media/ElvUI_Media.bat
+++ ElvUI_Media/ElvUI_Media.bat
@@ -1,41 +1,49 @@
 @echo off
-echo ElvUI Media
 echo Updating Media...
 
-echo local LSM = LibStub("LibSharedMedia-3.0") > ..\ElvUI_Media\ElvUI_Media.lua
+set LUA=ElvUI_Media.lua
 
-echo.>> ..\ElvUI_Media\ElvUI_Media.lua
-echo  --FONTS>> ..\ElvUI_Media\ElvUI_Media.lua
-for %%F in (..\ElvUI_Media\Fonts\*.*) do (
-    echo LSM:Register("font", "%%~nF", [[Interface\Addons\ElvUI_Media\Fonts\%%~nxF]])>> ..\ElvUI_Media\ElvUI_Media.lua
+echo local LSM = LibStub("LibSharedMedia-3.0") > "%LUA%"
+echo.>> "%LUA%"
+
+
+:: FONTS
+echo --FONTS>> "%LUA%"
+for %%F in ("Fonts\*.*") do (
+    >>"%LUA%" echo LSM:Register("font","%%~nF",[[Interface\Addons\ElvUI_Media\Fonts\%%~nxF"]])
 )
 
-echo.>> ..\ElvUI_Media\ElvUI_Media.lua
-echo  --STATUSBAR>> ..\ElvUI_Media\ElvUI_Media.lua
-for %%F in (..\ElvUI_Media\Statusbar\*.*) do (
-    echo LSM:Register("statusbar", "%%~nF", [[Interface\Addons\ElvUI_Media\Statusbar\%%~nxF]])>> ..\ElvUI_Media\ElvUI_Media.lua
+echo.>> "%LUA%"
+
+:: STATUSBAR
+echo --STATUSBAR>> "%LUA%"
+for %%F in ("Statusbar\*.*") do (
+    >>"%LUA%" echo LSM:Register("statusbar","%%~nF",[[Interface\Addons\ElvUI_Media\Statusbar\%%~nxF"]])
 )
 
-echo.>> ..\ElvUI_Media\ElvUI_Media.lua
-echo  --SOUNDS>> ..\ElvUI_Media\ElvUI_Media.lua
-for %%F in (..\ElvUI_Media\Sounds\*.*) do (
-    echo LSM:Register("sound", "%%~nF", [[Interface\Addons\ElvUI_Media\Sounds\%%~nxF]])>> ..\ElvUI_Media\ElvUI_Media.lua
+echo.>> "%LUA%"
+
+:: SOUNDS
+echo --SOUNDS>> "%LUA%"
+for %%F in ("Sounds\*.*") do (
+    >>"%LUA%" echo LSM:Register("sound","%%~nF",[[Interface\Addons\ElvUI_Media\Sounds\%%~nxF"]])
 )
 
-echo.>> ..\ElvUI_Media\ElvUI_Media.lua
-echo  --DETAILS>> ..\ElvUI_Media\ElvUI_Media.lua
-for %%F in (..\ElvUI_Media\Details\*.*) do (
-    echo LSM:Register("details", "%%~nF", [[Interface\Addons\ElvUI_Media\Details\%%~nxF]])>> ..\ElvUI_Media\ElvUI_Media.lua
+echo.>> "%LUA%"
+
+:: DETAILS
+echo --DETAILS>> "%LUA%"
+for %%F in ("Details\*.*") do (
+    >>"%LUA%" echo LSM:Register("details","%%~nF",[[Interface\Addons\ElvUI_Media\Details\%%~nxF"]])
 )
 
-echo.>> ..\ElvUI_Media\ElvUI_Media.lua
-echo  --ICONS>> ..\ElvUI_Media\ElvUI_Media.lua
-for %%F in (..\ElvUI_Media\Icons\*.*) do (
-    echo LSM:Register("icon", "%%~nF", [[Interface\Addons\ElvUI_Media\Icons\%%~nxF]])>> ..\ElvUI_Media\ElvUI_Media.lua
+echo.>> "%LUA%"
+
+:: ICONS
+echo --ICONS>> "%LUA%"
+for %%F in ("Icons\*.*") do (
+    >>"%LUA%" echo LSM:Register("icon","%%~nF",[[Interface\Addons\ElvUI_Media\Icons\%%~nxF"]])
 )
 
-echo.
-echo Everything is updated!
-echo.
+echo Done!
 pause
-
